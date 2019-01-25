97672a185cdcb59553d69880e69c0b22c91a7e94
soupette <cyril.lpz@gmail.com>
2019-01-25 14:22:47 +0100

    Fix hook

--- stories/Button/Button.stories.js
+++ stories/Button/Button.stories.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import Button from '../../src/components/Button';
+
 const stories = storiesOf('Button|Button', module);
 
-stories.add('primary', () => (
-  <Button>Yoo</Button>
-));
+stories.add('Button|Button', () => <Button>Yoo</Button>);
