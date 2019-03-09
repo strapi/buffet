66c7a38c8cde175e504e534bd2589538db918414
soupette <cyril.lpz@gmail.com>
2019-03-09 13:21:42 +0100

    Upgrade to SB5, jest, css-loader, all deps

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.{js,jsx}',
     '!src/**/*.test.{js,jsx}',
+    '!src/**/index.{js,jsx}',
     '!src/index.js',
     '!src/utils/*.{js,jsx}', // This should be tested
     '!src/assets/**/*.{js,jsx}',
@@ -24,7 +25,7 @@ module.exports = {
     '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/internals/mocks/image.js',
   },
-  setupTestFrameworkScriptFile: '<rootDir>/internals/testing/test-bundler.js',
+  setupFilesAfterEnv: ['<rootDir>/internals/testing/test-bundler.js'],
   setupFiles: ['raf/polyfill', '<rootDir>/internals/testing/enzyme-setup.js'],
   testRegex: 'tests/.*\\.test\\.js$',
 };
