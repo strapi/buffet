5144911cac8f19c711d3d4ca2d9ece4e66051754
Virginie Ky <virginieky@MacBook-Pro-de-Virginie-2.local>
2019-04-25 18:58:54 +0200

    snapshot update before PR

--- buffet/src/styled/Form/tests/Form.test.js
+++ buffet/src/styled/Form/tests/Form.test.js
@@ -1,11 +1,12 @@
-// import React from 'react';
-// import { mount } from 'enzyme';
-// import { enzymeFind } from 'styled-components/test-utils';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import Form from '../index';
+import Form from '../index';
+
+const renderComponent = () => shallow(<Form />);
 
 describe('<Form />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
+  it('should not crash', () => {
+    renderComponent();
   });
 });
