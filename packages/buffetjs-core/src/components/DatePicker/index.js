2f50bdcd3efbe81fde9a302ca089f891ea12b7a4
Virginie Ky <virginie.ky@gmail.com>
2019-12-06 14:57:37 +0100

    Date and time pickers zIndex

--- packages/buffetjs-core/src/components/DatePicker/index.js
+++ packages/buffetjs-core/src/components/DatePicker/index.js
@@ -79,7 +79,7 @@ class DatePicker extends React.PureComponent {
     const { date, isFocused, visible } = this.state;
 
     return (
-      <StyledDatepicker className={className}>
+      <StyledDatepicker className={`${className} ${visible && 'is-open'}`}>
         <div>
           <Input
             disabled={disabled}
