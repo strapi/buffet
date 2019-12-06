bbc607e79314a42e14f09d65c04ca1a9dabf275e
soupette <cyril.lpz@gmail.com>
2019-12-06 11:29:02 +0100

    Fix remove icon

--- packages/buffetjs-icons/src/components/Remove/index.js
+++ packages/buffetjs-icons/src/components/Remove/index.js
@@ -1,14 +1,21 @@
-import styled from 'styled-components';
-import Icon from './Icon';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Remove = styled(Icon)`
-  > path {
-    fill: #b3b5b9;
+const Icon = props => (
+  <svg width="8" height="8" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <g fill={props.fill} fillRule="nonzero">
+      <path d="M1.156 7.433c.06 0 .114-.023.16-.07l3.258-3.257a.221.221 0 000-.322L1.317.527a.221.221 0 00-.322 0l-.35.35a.221.221 0 000 .321l2.748 2.747L.646 6.692a.221.221 0 000 .322l.35.35c.046.046.1.07.16.07z" />
+      <path d="M6.888 7.433a.221.221 0 01-.16-.07L3.47 4.106a.221.221 0 010-.322L6.728.527a.221.221 0 01.321 0l.35.35a.221.221 0 010 .321L4.65 3.945 7.4 6.692a.221.221 0 010 .322l-.35.35a.221.221 0 01-.16.07z" />
+    </g>
+  </svg>
+);
 
-    &:hover {
-      fill: #aed4fb;
-    }
-  }
-`;
+Icon.defaultProps = {
+  fill: '#b3b5b9',
+};
 
-export default Remove;
+Icon.propTypes = {
+  fill: PropTypes.string,
+};
+
+export default Icon;
