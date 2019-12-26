741105ec2ebe69a242687ffdca66206b9152d0e5
soupette <cyril.lpz@gmail.com>
2019-12-26 14:25:49 +0100

    Fixes #68

--- packages/buffetjs-core/src/components/DatePicker/index.js
+++ packages/buffetjs-core/src/components/DatePicker/index.js
@@ -16,28 +16,45 @@ import { DatePicker as StyledDatepicker } from '@buffetjs/styles';
 import Input from '../InputText';
 
 class DatePicker extends React.PureComponent {
-  state = { date: null, isFocused: false, visible: false };
+  state = { date: null, displayedDate: '', isFocused: false, visible: false };
+
+  timer = null;
 
   componentDidMount() {
     const { value, withDefaultValue } = this.props;
     let date = null;
+    let displayedDate = '';
 
     if (withDefaultValue && !value) {
       date = moment();
+      displayedDate = date.format('MM/DD/YYYY');
     }
 
     if (!!value && moment(value).isValid()) {
       date = value._isAMomentObject === true ? value : moment(value);
+      displayedDate = date.format('MM/DD/YYYY');
     }
 
-    this.setState({ date });
+    this.setState({ date, displayedDate });
   }
+
+  handleChange = ({ target }) => {
+    this.setState({ displayedDate: target.value });
+
+    clearTimeout(this.timer);
+
+    this.timer = setTimeout(() => {
+      this.handleDateChange(moment(target.value));
+    }, this.props.wait);
+  };
 
   handleDateChange = date => {
     const { name, onChange } = this.props;
+
     if (moment(date).isValid()) {
-      this.setState({ date, visible: false }, () =>
-        onChange({ target: { name, type: 'date', value: date } })
+      this.setState(
+        { date, displayedDate: date.format('MM/DD/YYYY'), visible: true },
+        () => onChange({ target: { name, type: 'date', value: date } })
       );
     }
   };
@@ -74,9 +91,15 @@ class DatePicker extends React.PureComponent {
       displayFormat,
       id,
       name,
+      readOnly,
       tabIndex,
     } = this.props;
-    const { date, isFocused, visible } = this.state;
+    const { date, displayedDate, isFocused, visible } = this.state;
+    let dateValue = date ? moment(date).format(displayFormat) : '';
+
+    if (visible) {
+      dateValue = displayedDate;
+    }
 
     return (
       <StyledDatepicker className={`${className} ${visible && 'is-open'}`}>
@@ -86,8 +109,9 @@ class DatePicker extends React.PureComponent {
             type="text"
             name="start_date"
             id={id || name}
-            value={date ? moment(date).format(displayFormat) : ''}
-            readOnly
+            value={dateValue}
+            readOnly={readOnly}
+            onChange={this.handleChange}
             icon={<FontAwesomeIcon icon={faCalendarAlt} />}
             onClick={this.showDatepicker}
             onFocus={this.showDatepicker}
@@ -100,6 +124,7 @@ class DatePicker extends React.PureComponent {
             date={date}
             focused={isFocused}
             numberOfMonths={1}
+            keepOpenOnDateSelect
             onFocusChange={this.handleFocusChange}
             onDateChange={this.handleDateChange}
             onOutsideClick={this.handleOutsideClick}
@@ -116,8 +141,10 @@ DatePicker.defaultProps = {
   displayFormat: 'MMMM DD, YY',
   id: 'date',
   onChange: () => {},
+  readOnly: false,
   tabIndex: '0',
   value: null,
+  wait: 600,
   withDefaultValue: false,
 };
 
@@ -128,8 +155,10 @@ DatePicker.propTypes = {
   id: PropTypes.string,
   name: PropTypes.string.isRequired,
   onChange: PropTypes.func,
+  readOnly: PropTypes.bool,
   tabIndex: PropTypes.string,
   value: PropTypes.oneOfType([momentPropTypes.momentObj, PropTypes.string]),
+  wait: PropTypes.number,
   withDefaultValue: PropTypes.bool,
 };
 
