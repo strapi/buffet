963336133b425f066f1d729a78479348133cea82
Virginie Ky <virginie.ky@gmail.com>
2020-01-31 14:07:02 +0100

    remove useless import
    Signed-off-by: Virginie Ky <virginie.ky@gmail.com>

--- packages/buffetjs-styles/src/index.js
+++ packages/buffetjs-styles/src/index.js
@@ -1,4 +1,4 @@
-export { default as Button, buttonsColors } from './components/Button';
+export { default as Button } from './components/Button';
 export { default as Card } from './components/Card';
 export { default as Checkbox } from './components/Checkbox';
 export { default as CheckboxWrapper } from './components/CheckboxWrapper';
