be675041825d30810ce59fc1dcf56dde80172c2f
Virginie Ky <virginie.ky@gmail.com>
2020-02-05 18:29:24 +0100

    update datepicker tests
    Signed-off-by: Virginie Ky <virginie.ky@gmail.com>

--- packages/buffetjs-core/src/components/DatePicker/tests/index.test.js
+++ packages/buffetjs-core/src/components/DatePicker/tests/index.test.js
@@ -1,47 +1,104 @@
 import React from 'react';
 import { mount } from 'enzyme';
-// import moment from 'moment';
-// import { Input } from '@buffetjs/core';
+import moment from 'moment';
+import { DayPickerSingleDateController } from 'react-dates';
+import { act, cleanup, render } from '@testing-library/react';
 
+import InputText from '../../InputText';
 import DatePicker from '../index';
 
+jest.useFakeTimers();
+
 const defaultProps = {
-  type: 'checkbox',
-  name: 'checkbox',
+  type: 'date',
+  name: 'date',
   onChange: jest.fn(),
 };
 const renderComponent = (props = defaultProps) =>
   mount(<DatePicker {...props} />);
 
 describe('<DatePicker />', () => {
+  afterEach(cleanup);
+
   it('should not crash', () => {
     renderComponent();
   });
 
-  // it('should send a moment object onChange', () => {
-  //   const onChange = jest.fn();
+  it('should match snapshot', () => {
+    const { asFragment } = render(<DatePicker {...defaultProps} />);
 
-  //   const renderedComponent = renderComponent({
-  //     ...defaultProps,
-  //     onChange,
-  //     value: null,
-  //   });
+    expect(asFragment()).toMatchSnapshot();
+  });
 
-  //   const element = renderedComponent.find(Input);
+  it('should render an input text', () => {
+    const wrapper = renderComponent();
 
-  //   const mock = { target: { value: moment('1993-07-05') } };
-  //   element.simulate('change', mock);
+    const input = wrapper.find(InputText);
+    expect(input).toHaveLength(1);
+  });
 
-  //   const expected = {
-  //     target: {
-  //       name: 'date',
-  //       type: 'date',
-  //       value: moment('1993-07-05'),
-  //     },
-  //   };
+  it('should render a date picker on input text click', () => {
+    const onChange = jest.fn();
 
-  //   expect(onChange).toHaveBeenCalledWith(expected);
-  // });
+    const props = {
+      ...defaultProps,
+      onChange,
+      value: moment(new Date(), 'YYYY-MM-DD'),
+    };
+
+    const wrapper = renderComponent(props);
+    const input = wrapper.find('input');
+
+    input.simulate('click');
+
+    const datePicker = wrapper.find(DayPickerSingleDateController);
+
+    expect(datePicker).toHaveLength(1);
+  });
+
+  it('should render onChange props on datepicker select', () => {
+    const onChange = jest.fn();
+
+    const props = {
+      ...defaultProps,
+      onChange,
+      value: moment(new Date(), 'YYYY-MM-DD'),
+    };
+
+    const wrapper = renderComponent(props);
+    const input = wrapper.find('input');
+
+    input.simulate('click');
+
+    const datePicker = wrapper.find(DayPickerSingleDateController);
+
+    act(() => {
+      datePicker.props().onDateChange(moment('1993-07-05', 'YYYY-MM-DD'));
+    });
+
+    expect(onChange).toHaveBeenCalledWith({
+      target: {
+        name: 'date',
+        type: 'date',
+        value: moment('1993-07-05', 'YYYY-MM-DD'),
+      },
+    });
+  });
+
+  it('should display the date value in a human readable format', () => {
+    const onChange = jest.fn();
+
+    const props = {
+      ...defaultProps,
+      onChange,
+      value: moment('1993-07-05', 'YYYY-MM-DD'),
+    };
+
+    const wrapper = renderComponent(props);
+    const input = wrapper.find('input');
+
+    expect(input.props().value).toBe('July 05, 93');
+  });
 
   it('should use the defaultProps', () => {
     const {
