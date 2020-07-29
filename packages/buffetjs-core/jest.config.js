f1aaf3fc6aa4f94e922658dfbb7f921c731a7ac7
soupette <cyril.lpz@gmail.com>
2020-07-29 17:23:40 +0200

    Add test setup for core
    Signed-off-by: soupette <cyril.lpz@gmail.com>

---
+++ packages/buffetjs-core/jest.config.js
@@ -0,0 +1,9 @@
+const rootConf = require('../../tools-configuration/jest.config.js');
+
+module.exports = Object.assign({}, rootConf, {
+  moduleNameMapper: {
+    ...rootConf.moduleNameMapper,
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/OLD/', '/dist/', '/build/'],
+  setupFilesAfterEnv: ['./test-bundler.js'],
+});
--- tools-configuration/jest.config.js
+++ tools-configuration/jest.config.js
@@ -5,7 +5,10 @@ module.exports = {
       '<rootDir>../../tools-configuration/mocks/styleMock.js',
     '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$':
       '<rootDir>../../tools-configuration/mocks/image.js',
-    '@buffet-js(.*)': '<rootDir>..$1',
+    '@buffetjs/icons': '<rootDir>../../packages/buffetjs-icons/src/index',
+    '@buffetjs/hooks': '<rootDir>../../packages/buffetjs-hooks/src/index',
+    '@buffetjs/styles': '<rootDir>../../packages/buffetjs-styles/src/index',
+    '@buffetjs/utils': '<rootDir>../../packages/buffetjs-utils/src/index',
   },
   setupFiles: ['jest-canvas-mock'],
   setupFilesAfterEnv: ['<rootDir>../../tools-configuration/test-bundler.js'],
