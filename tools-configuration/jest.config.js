b7cc4706fb0f9bf2639e3a3192679a4626bdeb65
soupette <cyril.lpz@gmail.com>
2020-07-30 09:21:24 +0200

    Fix custom test setup
    Signed-off-by: soupette <cyril.lpz@gmail.com>

--- tools-configuration/jest.config.js
+++ tools-configuration/jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
       '<rootDir>../../tools-configuration/mocks/styleMock.js',
     '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$':
       '<rootDir>../../tools-configuration/mocks/image.js',
+    '@buffetjs/core': '<rootDir>../../packages/buffetjs-core/src/index',
     '@buffetjs/icons': '<rootDir>../../packages/buffetjs-icons/src/index',
     '@buffetjs/hooks': '<rootDir>../../packages/buffetjs-hooks/src/index',
     '@buffetjs/styles': '<rootDir>../../packages/buffetjs-styles/src/index',
