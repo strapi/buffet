81a1d5e1648efd781ab71328789bc7e1574f135a
HichamELBSI <elabbassih@gmail.com>
2021-01-04 10:44:35 +0100

    Fix picker component
    Signed-off-by: HichamELBSI <elabbassih@gmail.com>

--- docs/stories/components/Picker.stories.js
+++ docs/stories/components/Picker.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/react';
 import { Picker, Padded, Text, Button } from '@buffetjs/core';
+import { Arrow } from '@buffetjs/icons';
 
 import Presentation from '../ui/Presentation';
 import Pre from '../ui/Pre';
@@ -16,25 +17,54 @@ const styles = {
 const PickerStory = () => (
   <Presentation title="Picker" description="A simple dropdown picker">
     <div style={{ paddingTop: 17, marginBottom: 22 }}>
-      <Picker
-        renderButtonContent={isOpen => (
-          <Text>Picker button {isOpen ? 'open' : 'close'}</Text>
-        )}
-        renderSectionContent={onToggle => (
-          <>
-            <Padded top left right bottom>
-              <Text>Picker content</Text>
-            </Padded>
-            <Padded top left right bottom>
-              <Button onClick={onToggle}>
-                <Text color="white" fontSize="md">
-                  Close
-                </Text>
-              </Button>
-            </Padded>
-          </>
-        )}
-      />
+      <div style={{ display: 'flex' }}>
+        <Picker
+          renderButtonContent={(isOpen) => (
+            <>
+              <Arrow fill="black" />
+              <Text>Sort {isOpen ? 'open' : 'close'}</Text>
+            </>
+          )}
+          renderSectionContent={(onToggle) => (
+            <>
+              <Padded top left right bottom>
+                <Text>Picker content</Text>
+              </Padded>
+              <Padded top left right bottom>
+                <Button onClick={onToggle}>
+                  <Text color="white" fontSize="md">
+                    Close
+                  </Text>
+                </Button>
+              </Padded>
+            </>
+          )}
+        />
+        <div style={{ paddingLeft: '10px' }} />
+
+        <Picker
+          renderButtonContent={(isOpen) => (
+            <>
+              <Arrow fill="black" />
+              <Text>Filter {isOpen ? 'open' : 'close'}</Text>
+            </>
+          )}
+          renderSectionContent={(onToggle) => (
+            <>
+              <Padded top left right bottom>
+                <Text>Picker content</Text>
+              </Padded>
+              <Padded top left right bottom>
+                <Button onClick={onToggle}>
+                  <Text color="white" fontSize="md">
+                    Close
+                  </Text>
+                </Button>
+              </Padded>
+            </>
+          )}
+        />
+      </div>
 
       <span style={styles.spacerCode} />
       <Pre language="Pre">
